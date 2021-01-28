4acb89ee60d8b02589385511c532c186b8f2ca87
Batool AlObaid <batool-998@hotmail.com>
2021-01-29 02:21:07 +0300

    docs: Update docstrings of time_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -94,7 +94,13 @@ def load_data(city, month, day):
     return df
 
 def time_stats(df,month,day):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+
+       Args:
+           (DataFrame) df - name of the dataframe to analyze
+           (str) month - name of the month to filter by, or "all" to apply no month filter
+           (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
